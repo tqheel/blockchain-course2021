591ed50726f2ff0c9870d1139399d3d8fa211cdc
Todd E. Qualls <tqualls@gmail.com>
2021-11-28 14:02:50 -0500

    Adds tests for Block class.

---
+++ cryptochain/block.test.js
@@ -0,0 +1,41 @@
+const Block = require("./block");
+const { GENESIS_DATA } = require("./config");
+
+describe('Block', () => {
+    const  timestamp = 'a-date';
+    const lastHash = 'foo-hash';
+    const hash = 'bar-hash';
+    const data = ['blockchain', 'data'];
+    const block = new Block({
+        timestamp: timestamp,
+        lastHash: lastHash,
+        data: data,
+        hash: hash
+    });
+    it('has a timestamp, lastHash, hash, and data properties', () => {
+        expect(block.timestamp).toEqual(timestamp);
+        expect(block.lastHash).toEqual(lastHash);
+        expect(block.hash).toEqual(hash);
+        expect(block.data).toEqual(data);
+
+    });
+
+    describe('genesis()', () => {
+        const genesisBlock = Block.genesis();
+
+        it('returns a Block instance', () => {
+            expect(genesisBlock instanceof Block).toBe(true);
+        });
+
+        it('returns the genesis data', () => {
+            expect(genesisBlock).toEqual(GENESIS_DATA);
+        })
+    });
+
+    describe('mineBlock()', () => {
+        const lastBlock = Block.genesis();
+        const data = 'mined data';
+        const mineBlock = Block.mine(lastBlock, data);
+    });
+
+});
