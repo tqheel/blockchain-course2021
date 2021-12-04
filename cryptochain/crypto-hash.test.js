a03a8a4b507f8df63d925c8bce24d00645bc943e
Todd Qualls <tqualls@gmail.com>
2021-12-04 09:32:20 -0500

    Create hash function and tests.

--- cryptochain/crypto-hash.test.js
+++ cryptochain/crypto-hash.test.js
@@ -1,5 +1,14 @@
-const cryptoHash = require('./crypto-hah');
+const cryptoHash = require('./crypto-hash');
 
 describe('cryptoHash()', () => {
+    
+    it('generates a SHA-256 hashed output', () => {
+        expect(cryptoHash('foo'))
+            .toEqual('2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae');
+    });
 
+    it('produces the same hash wiht the same input arguments in any order', () => {
+        expect(cryptoHash('one', 'two', 'three'))
+            .toEqual(cryptoHash('three', 'one', 'two'));
+    });
 });
